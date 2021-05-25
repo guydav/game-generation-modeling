d2c58e83b49419a7ff7b7d08178378edeb50cb0f
Guy Davidson <guydav@gmail.com>
2021-05-24 20:36:13 -0400

    Printing validation test code cleanup.
    Cleaned up the code for the printing validation test.
    Created an `ASTPrinter` class, and configured an instance to parse each component of the DSL.
    All games currently pass the text => AST => text => AST => text test.

--- parse_dsl.py
+++ parse_dsl.py
@@ -72,13 +72,13 @@ def _parse_variable_list(var_list):
 QUANTIFIER_KEYS = ('args', 'pred', 'then')
 
 
-def _handle_quantifier(caller, name, ast, depth, increment, arg_keys=QUANTIFIER_KEYS):
-    formatted_vars = _parse_variable_list(ast[f'{name}_vars'][1])
-    _indent_print(f'({name} ({" ".join(formatted_vars)})', depth, increment)
+def _handle_quantifier(caller, rule, ast, depth, increment):
+    formatted_vars = _parse_variable_list(ast[f'{rule}_vars'][1])
+    _indent_print(f'({rule} ({" ".join(formatted_vars)})', depth, increment)
 
     found_args = False
-    for key in arg_keys:
-        key_str = f'{name}_{key}'
+    for key in QUANTIFIER_KEYS:
+        key_str = f'{rule}_{key}'
         if key_str in ast:
             found_args = True
             caller(ast[key_str], depth + 1)
@@ -89,43 +89,47 @@ def _handle_quantifier(caller, name, ast, depth, increment, arg_keys=QUANTIFIER_
     _indent_print(')', depth, increment)
 
 
-def _handle_logical(caller, name, ast, depth, increment):
-    _indent_print(f'({name}', depth, increment)
-    caller(ast[f'{name}_args'], depth + 1)
+def _handle_logical(caller, rule, ast, depth, increment):
+    _indent_print(f'({rule}', depth, increment)
+    caller(ast[f'{rule}_args'], depth + 1)
     _indent_print(f')', depth, increment)
 
 
-def _handle_game(name, ast, depth, increment):
-    _indent_print(f'({name.replace("_", "-")}', depth, increment)
-    parse_setup(ast[f'{name.replace("game_", "")}_pred'], depth + 1)
+def _handle_game(caller, rule, ast, depth, increment):
+    _indent_print(f'({rule.replace("_", "-")}', depth, increment)
+    caller(ast[f'{rule.replace("game_", "")}_pred'], depth + 1)
     _indent_print(f')', depth, increment)
 
 
-def _format_function_eval(ast):
+def _handle_function_eval(caller, rule, ast, depth, increment):
+    _indent_print(f'({ast.func_name} {ast.func_args and " ".join(ast.func_args) or ""})', depth, increment)
+
+
+def _inline_format_function_eval(ast):
     return f'({ast.func_name} {" ".join(ast.func_args)})'
 
 
-def _handle_function_comparison(ast, depth, increment):
+def _handle_function_comparison(caller, rule, ast, depth, increment):
     comp_op = '='
     if 'comp_op' in ast:
         comp_op = ast.comp_op
 
     if 'comp_func_1' in ast:
-        args = [_format_function_eval(ast.comp_func_1), _format_function_eval(ast.comp_func_2)]
+        args = [_inline_format_function_eval(ast.comp_func_1), _inline_format_function_eval(ast.comp_func_2)]
 
     elif 'comp_func_first' in ast:
-        args = [_format_function_eval(ast.comp_func_first), ast.comp_num]
+        args = [_inline_format_function_eval(ast.comp_func_first), ast.comp_num]
 
     elif 'comp_func_second' in ast:
-        args = [ast.comp_num, _format_function_eval(ast.comp_func_second)]
+        args = [ast.comp_num, _inline_format_function_eval(ast.comp_func_second)]
 
     else:
-        args = [_format_function_eval(func) for func in ast.equal_comp_funcs]
+        args = [_inline_format_function_eval(func) for func in ast.equal_comp_funcs]
 
     _indent_print(f'({comp_op} {" ".join(args)})', depth, increment)
 
 
-def _handle_predicate(ast, depth, increment, return_str=False):
+def _handle_predicate(caller, rule, ast, depth, increment, return_str=False):
     name = ast.pred_name
     args = []
     if ast.pred_args:
@@ -136,7 +140,7 @@ def _handle_predicate(ast, depth, increment, return_str=False):
                 if isinstance(arg, str):
                     args.append(arg)
                 else:
-                    args.append(_handle_predicate(arg, depth + 1, increment, return_str=True))
+                    args.append(_handle_predicate(caller, rule, arg, depth + 1, increment, return_str=True))
 
     out = f'({name} {" ".join(args)})'
     if return_str:
@@ -145,277 +149,229 @@ def _handle_predicate(ast, depth, increment, return_str=False):
     _indent_print(out, depth, increment)
 
 
-def _check_simple_cases(caller, ast, depth, increment):
-    if not ast:
-        return True
+class ASTPrinter:
+    def __init__(self, ast_key, rule_name_substitutions):
+        self.ast_key = ast_key
+        self.rule_name_substitutions = rule_name_substitutions
+        self.exact_matches = {}
+        self.keyword_matches = []
 
-    if isinstance(ast, tuple):
-        _indent_print(ast[0], depth, increment)
-        caller(ast[1], depth + 1, increment)
-        if len(ast) > 2:
-            if len(ast) > 3:
-                raise ValueError(f'Unexpectedly long tuple: {ast}')
-            
-            _indent_print(ast[2], depth, increment)
+    def register_exact_matches(self, *handlers):
+        for handler in handlers:
+            self.register_exact_match(handler)
 
-        return True
+    def register_exact_match(self, handler, rule_name=None):
+        if rule_name is None:
+            rule_name = handler.__name__.replace('_handle_', '')
+        self.exact_matches[rule_name] = handler
 
-    if isinstance(ast, list):
-        for item in ast:
-            caller(item, depth, increment)
-        return True
+    def register_keyword_match(self, keywords, handler):
+        self.keyword_matches.append((keywords, handler))
 
-    if isinstance(ast, str):
-        _indent_print(ast, depth, increment)
-        return True
+    def _parse_base_cases(self, ast, depth, increment):
+        if not ast:
+            return True
 
-    return False
+        if isinstance(ast, tuple):
+            _indent_print(ast[0], depth, increment)
+            self(ast[1], depth + 1, increment)
+            if len(ast) > 2:
+                if len(ast) > 3:
+                    raise ValueError(f'Unexpectedly long tuple: {ast}')
+                
+                _indent_print(ast[2], depth, increment)
+
+            return True
+
+        if isinstance(ast, list):
+            for item in ast:
+                self(item, depth, increment)
+            return True
+
+        if isinstance(ast, str):
+            _indent_print(ast, depth, increment)
+            return True
+
+        return False
+
+    def __call__(self, ast, depth=0, increment=DEFAULT_INCREMENT):
+        if self._parse_base_cases(ast, depth, increment):
+            return
+
+        rule = ast.parseinfo.rule
+        for sub in self.rule_name_substitutions:
+            rule = rule.replace(sub, '')
+
+        found_match = False
+
+        if rule in self.exact_matches:
+            found_match  = True
+            self.exact_matches[rule](self, rule, ast, depth, increment)
+
+        else:
+            for keywords, handler in self.keyword_matches:
+                if any([keyword in rule for keyword in keywords]):
+                    found_match = True
+                    handler(self, rule, ast, depth, increment)
+                    break
+
+        if not found_match:
+            raise ValueError(f'No match found in {self.ast_key} for: {ast}')
 
 
-def parse_setup(ast, depth=0, increment=DEFAULT_INCREMENT):
-    if _check_simple_cases(parse_setup, ast, depth, increment):
-        return
-
-    rule = ast.parseinfo.rule.replace('setup_', '')
-
-    if rule == 'function_comparison':
-        _handle_function_comparison(ast, depth, increment)
-
-    elif rule == 'predicate':
-        _handle_predicate(ast, depth, increment)
-    
-    elif 'exists' in rule or 'forall' in rule:
-        _handle_quantifier(parse_setup, rule, ast, depth, increment)
-
-    elif 'and' in rule or 'or' in rule or 'not' in rule:
-        _handle_logical(parse_setup, rule.replace('_predicate', ''), ast, depth, increment)
-
-    elif 'game' in rule:
-        _handle_game(rule, ast, depth, increment)
-    
-    else:
-        raise ValueError(f'Found unknown ast element: {ast}')
+def build_setup_printer():
+    printer = ASTPrinter('(:setup', ('setup_', '_predicate'))
+    printer.register_exact_matches(
+        _handle_function_comparison, _handle_predicate, _handle_function_eval)
+    printer.register_keyword_match(('exists', 'forall'), _handle_quantifier)
+    printer.register_keyword_match(('game',), _handle_game)
+    printer.register_keyword_match(('and', 'or', 'not'), _handle_logical)
+    return printer
 
 
-def _handle_preference(ast, depth, increment):
+def _handle_preference(caller, rule, ast, depth, increment):
     _indent_print(f'(preference {ast.pref_name}', depth, increment)
-    parse_constraints(ast.pref_body, depth + 1, increment)
+    caller(ast.pref_body, depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_then(ast, depth, increment):
+def _handle_then(caller, rule, ast, depth, increment):
     _indent_print(f'(then', depth, increment)
-    parse_constraints(ast.then_funcs, depth + 1, increment)
+    caller(ast.then_funcs, depth + 1, increment)
     _indent_print(f')', depth, increment)
 
 
-def _handle_at_end(ast, depth, increment):
+def _handle_at_end(caller, rule, ast, depth, increment):
     _indent_print(f'(at-end', depth, increment)
-    parse_constraints(ast.at_end_pred, depth + 1, increment)
+    caller(ast.at_end_pred, depth + 1, increment)
     _indent_print(f')', depth, increment)
 
 
-def _handle_once(ast, depth, increment):
+def _handle_any(caller, rule, ast, depth, increment):
+    _indent_print('(any)', depth, increment)
+
+
+def _handle_once(caller, rule, ast, depth, increment):
     _indent_print('(once', depth, increment)
-    parse_constraints(ast.once_pred, depth + 1, increment)
+    caller(ast.once_pred, depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_once_measure(ast, depth, increment):
+def _handle_once_measure(caller, rule, ast, depth, increment):
     _indent_print('(once-measure', depth, increment)
-    parse_constraints(ast.once_measure_pred, depth + 1, increment)
-    _indent_print(_format_function_eval(ast.measurement), depth + 1, increment)
+    caller(ast.once_measure_pred, depth + 1, increment)
+    _indent_print(_inline_format_function_eval(ast.measurement), depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_hold(ast, depth, increment):
+def _handle_hold(caller, rule, ast, depth, increment):
     _indent_print('(hold', depth, increment)
-    parse_constraints(ast.hold_pred, depth + 1, increment)
+    caller(ast.hold_pred, depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_while_hold(ast, depth, increment):
+def _handle_while_hold(caller, rule, ast, depth, increment):
     _indent_print('(hold-while', depth, increment)
-    parse_constraints([ast.hold_pred, ast.while_preds], depth + 1, increment)
+    caller([ast.hold_pred, ast.while_preds], depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_hold_for(ast, depth, increment):
+def _handle_hold_for(caller, rule, ast, depth, increment):
     _indent_print(f'(hold-for {ast.num_to_hold}', depth, increment)
-    parse_constraints(ast.hold_pred, depth + 1, increment)
+    caller(ast.hold_pred, depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_hold_to_end(ast, depth, increment):
+def _handle_hold_to_end(caller, rule, ast, depth, increment):
     _indent_print('(hold-to-end', depth, increment)
-    parse_constraints(ast.hold_pred, depth + 1, increment)
+    caller(ast.hold_pred, depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_forall_seq(ast, depth, increment):
+def _handle_forall_seq(caller, rule, ast, depth, increment):
     formatted_vars = _parse_variable_list(ast.forall_seq_vars[1])
     _indent_print(f'(forall-sequence ({" ".join(formatted_vars)})', depth, increment)
-    parse_constraints(ast.forall_seq_then, depth + 1, increment)
+    caller(ast.forall_seq_then, depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def parse_constraints(ast, depth=0, increment=DEFAULT_INCREMENT):
-    if _check_simple_cases(parse_constraints, ast, depth, increment):
-        return
-
-    if isinstance(ast, tuple) and ast[0] == '(and':
-        _indent_print(ast[0], depth, increment)
-        parse_constraints(ast[1], depth + 1)
-        _indent_print(ast[2], depth, increment)
-        return
-
-    rule = ast.parseinfo.rule.replace('pref_body_', '').replace('pref_', '').replace('predicate_', '')
-
-    if rule == 'preference':
-        _handle_preference(ast, depth, increment)
-
-    elif rule == 'function_comparison':
-        _handle_function_comparison(ast, depth, increment)
-
-    elif rule == 'predicate':
-        _handle_predicate(ast, depth, increment)
-    
-    elif rule == 'at_end':
-        _handle_at_end(ast, depth, increment)
-
-    elif rule == 'then':
-        _handle_then(ast, depth, increment)
-
-    elif rule == 'any':
-        _indent_print('(any)', depth, increment)
-
-    elif rule == 'once':
-        _handle_once(ast, depth, increment)
-
-    elif rule == 'once_measure':
-        _handle_once_measure(ast, depth, increment)
-
-    elif rule == 'hold':
-        _handle_hold(ast, depth, increment)
-
-    elif rule == 'while_hold':
-        _handle_while_hold(ast, depth, increment)
-
-    elif rule == 'hold_for':
-        _handle_hold_for(ast, depth, increment)
-
-    elif rule == 'hold_to_end':
-        _handle_hold_to_end(ast, depth, increment)
-
-    elif rule == 'forall_seq':
-        _handle_forall_seq(ast, depth, increment)
-
-    elif 'exists' in rule or 'forall' in rule:
-        _handle_quantifier(parse_constraints, rule, ast, depth, increment)
-
-    elif 'and' in rule or 'or' in rule or 'not' in rule:
-        _handle_logical(parse_constraints, rule, ast, depth, increment)
-
-    else:
-        raise ValueError(f'Found unknown ast element: {ast}')
+def build_constraints_printer():
+    printer = ASTPrinter('(:constraints', ('pref_body_', 'pref_', 'predicate_'))
+    printer.register_exact_matches(
+        _handle_preference, _handle_function_comparison, _handle_predicate,
+        _handle_at_end, _handle_then, _handle_any, _handle_once, _handle_once_measure,
+        _handle_hold, _handle_while_hold, _handle_hold_for, _handle_hold_to_end, 
+        _handle_forall_seq, _handle_function_eval
+    )
+    printer.register_keyword_match(('exists', 'forall'), _handle_quantifier)
+    printer.register_keyword_match(('and', 'or', 'not'), _handle_logical)
+    return printer
 
 
-def _handle_binary_comp(caller, ast, depth, increment):
+def _handle_binary_comp(caller, rule, ast, depth, increment):
     _indent_print(f'({ast.op}', depth, increment)
     caller([ast.expr_1, ast.expr_2], depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_multi_expr(caller, ast, depth, increment):
+def _handle_multi_expr(caller, rule, ast, depth, increment):
     _indent_print(f'({ast.op}', depth, increment)
     caller(ast.expr, depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_binary_expr(caller, ast, depth, increment):
+def _handle_binary_expr(caller, rule, ast, depth, increment):
     _indent_print(f'({ast.op}', depth, increment)
     caller([ast.expr_1, ast.expr_2], depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_neg_expr(caller, ast, depth, increment):
+def _handle_neg_expr(caller, rule, ast, depth, increment):
     _indent_print('(-', depth, increment)
     caller(ast.expr, depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_equals_comp(caller, ast, depth, increment):
+def _handle_equals_comp(caller, rule, ast, depth, increment):
     _indent_print('(=', depth, increment)
     caller(ast.expr, depth + 1, increment)
     _indent_print(')', depth, increment)
 
 
-def _handle_preference_eval(caller, ast, depth, increment):
+def _handle_preference_eval(caller, rule, ast, depth, increment):
     _indent_print(f'({ast.parseinfo.rule.replace("_", "-")} {ast.pref_name})', depth, increment)
 
 
-def parse_terminal(ast, depth=0, increment=DEFAULT_INCREMENT):
-    if _check_simple_cases(parse_terminal, ast, depth, increment):
-        return
-
-    rule = ast.parseinfo.rule.replace('terminal_', '').replace('scoring_', '')
-
-    if rule == 'comp':
-        _handle_binary_comp(parse_terminal, ast, depth, increment)
-
-    elif rule == 'multi_expr':
-        _handle_multi_expr(parse_terminal, ast, depth, increment)
-
-    elif rule == 'binary_expr':
-        _handle_binary_expr(parse_terminal, ast, depth, increment)
-
-    elif rule == 'neg_expr':
-        _handle_neg_expr(parse_terminal, ast, depth, increment)
-
-    elif rule == 'equals_comp':
-        _handle_equals_comp(parse_terminal, ast, depth, increment)
-
-    elif 'and' in rule or 'or' in rule or 'not' in rule:
-        _handle_logical(parse_terminal, rule, ast, depth, increment)
-    
-    elif 'count' in rule:
-        _handle_preference_eval(parse_terminal, ast, depth, increment)
+def build_terminal_printer():
+    printer = ASTPrinter('(:terminal', ('terminal_', 'scoring_'))
+    printer.register_exact_matches(
+        _handle_multi_expr, _handle_binary_expr, _handle_neg_expr, 
+        _handle_equals_comp, _handle_function_eval
+    )
+    printer.register_exact_match(_handle_binary_comp, 'comp')
+    printer.register_keyword_match(('count',), _handle_preference_eval)
+    printer.register_keyword_match(('and', 'or', 'not'), _handle_logical)
+    return printer
 
 
+def build_scoring_printer():
+    printer = ASTPrinter('(:scoring', ('scoring_',))
+    printer.register_exact_matches(
+        _handle_multi_expr, _handle_binary_expr, _handle_neg_expr, 
+        _handle_equals_comp, _handle_function_eval
+    )
+    printer.register_exact_match(_handle_binary_comp, 'comp')
+    printer.register_keyword_match(('count',), _handle_preference_eval)
+    printer.register_keyword_match(('and', 'or', 'not'), _handle_logical)
+    return printer
 
-def parse_scoring(ast, depth=0, increment=DEFAULT_INCREMENT):
-    if _check_simple_cases(parse_scoring, ast, depth, increment):
-        return
-
-    rule = ast.parseinfo.rule.replace('scoring_', '')
-
-    if 'count' in rule:
-        _handle_preference_eval(parse_scoring, ast, depth, increment)
-
-    elif rule == 'comp':
-        _handle_binary_comp(parse_scoring, ast, depth, increment)
-
-    elif rule == 'multi_expr':
-        _handle_multi_expr(parse_scoring, ast, depth, increment)
-
-    elif rule == 'binary_expr':
-        _handle_binary_expr(parse_scoring, ast, depth, increment)
-
-    elif rule == 'neg_expr':
-        _handle_neg_expr(parse_scoring, ast, depth, increment)
-
-    elif rule == 'equals_comp':
-        _handle_equals_comp(parse_scoring, ast, depth, increment)
-
-    elif 'and' in rule or 'or' in rule or 'not' in rule:
-        _handle_logical(parse_scoring, rule, ast, depth, increment)
 
 
 PARSE_DICT = {
-    '(:setup': parse_setup,
-    '(:constraints': parse_constraints,
-    '(:terminal': parse_terminal,
-    '(:scoring': parse_scoring,
+    '(:setup': build_setup_printer(),
+    '(:constraints': build_constraints_printer(),
+    '(:terminal': build_terminal_printer(),
+    '(:scoring': build_scoring_printer(),
 }
 
 

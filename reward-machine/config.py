282aae2d577cd371a8e08c5c7fd039acfade5e25
Graham Todd <gdrtodd@gmail.com>
2022-07-29 14:46:48 -0400

    Add simple predicate evaluation for reworked parser

--- reward-machine/config.py
+++ reward-machine/config.py
@@ -1,5 +1,32 @@
+import numpy as np
+
+# ====================================== PREDICATE DEFINITIONS ======================================
+def _agent_crouches(state):
+    return state["objects"]["agent"]["is_crouching"]
+
+def _agent_holds(state, obj):
+    return state["objects"]["agent"]["holding"] == obj["name"]
+
+def _in(state, obj1, obj2):
+    return all([obj1["position"][i] == obj2["position"][i] for i in range(3)]) 
+
+def _in_motion(state, obj):
+    return np.linalg.norm(obj["velocity"]) > 0
+# ===================================================================================================
+
+# ====================================== FUNCTION DEFINITIONS =======================================
+
+# TODO
+
+# ===================================================================================================
 
 OBJECTS_BY_TYPE = {"ball": ["blue-dodgeball-1", "red-dodgeball-1", "pink-dodgeball-1"],
                    "bin": ["hexagonal-bin-1", "hexagonal-bin-2"],
                    "wall": ["left-wall-1", "right-wall-1", "front-wall-1", "back-wall-1"],
                    "agent": ["agent"]}
+
+PREDICATE_LIBRARY = {"agent_crouches": _agent_crouches,
+                     "agent_holds": _agent_holds,
+                     "in": _in,
+                     "in_motion": _in_motion}
+
--- reward-machine/preference_handler.py
+++ reward-machine/preference_handler.py
@@ -6,7 +6,7 @@ import tatsu
 
 import numpy as np
 
-from config import OBJECTS_BY_TYPE
+from config import OBJECTS_BY_TYPE, PREDICATE_LIBRARY
 
 class PreferenceHandler():
     def __init__(self, preference):
@@ -17,7 +17,7 @@ class PreferenceHandler():
         body = preference["definition"]["pref_body"]["body"]
 
         # Extract the mapping of variable names to types (e.g. {?d : dodgeball})
-        self.variable_mapping = self._extract_variables(body["exists_vars"]["variables"])
+        self.variable_mapping = self._extract_variable_mapping(body["exists_vars"]["variables"])
         self.variable_mapping["agent"] = "agent"
 
         # Extract the ordered list of temporal predicates
@@ -33,16 +33,16 @@ class PreferenceHandler():
         #      ]
         self.predicate_states = []
 
-        initial_arguments = self._extract_arguments(self.temporal_predicates[0])
-        initial_arg_types = [self.variable_mapping[arg] for arg in initial_arguments]
+        initial_variables = self._extract_variables(self.temporal_predicates[0])
+        initial_arg_types = [self.variable_mapping[arg] for arg in initial_variables]
         object_assignments = list(itertools.product(*[OBJECTS_BY_TYPE[arg_type] for arg_type in initial_arg_types]))
 
         for object_assignment in object_assignments:
-            mapping = dict(zip(initial_arguments, object_assignment))
+            mapping = dict(zip(initial_variables, object_assignment))
             self.predicate_states.append((mapping, 0))
 
 
-    def _extract_variables(self, variable_list):
+    def _extract_variable_mapping(self, variable_list):
         if isinstance(variable_list, tatsu.ast.AST):
             variable_list = [variable_list]
 
@@ -52,34 +52,34 @@ class PreferenceHandler():
 
         return variables
 
-    def _extract_arguments(self, predicate):
+    def _extract_variables(self, predicate):
         '''
         Recursively extract every variable referenced in the predicate (including inside functions 
         used within the predicate)
         '''
 
         if isinstance(predicate, list) or isinstance(predicate, tuple):
-            pred_args = []
+            pred_vars = []
             for sub_predicate in predicate:
-                pred_args += self._extract_arguments(sub_predicate)
+                pred_vars += self._extract_variables(sub_predicate)
 
-            return list(set(pred_args))
+            return list(set(pred_vars))
 
         elif isinstance(predicate, tatsu.ast.AST):
-            pred_args = []
+            pred_vars = []
             for key in predicate:
                 if key == "term":
 
                     # Different structure for predicate args vs. function args
                     if isinstance(predicate["term"], tatsu.ast.AST):
-                        pred_args += [predicate["term"]["arg"]]
+                        pred_vars += [predicate["term"]["arg"]]
                     else:
-                        pred_args += [predicate["term"]]
+                        pred_vars += [predicate["term"]]
 
                 elif key != "parseinfo":
-                    pred_args += self._extract_arguments(predicate[key])
+                    pred_vars += self._extract_variables(predicate[key])
 
-            return list(set(pred_args))
+            return list(set(pred_vars))
 
         else:
             return []
@@ -113,7 +113,44 @@ class PreferenceHandler():
         for mapping, internal_state in self.predicate_states:
             current_predicate = self.temporal_predicates[internal_state]
             pred_type = self._type(current_predicate)
-            print(pred_type)
+            
+            print(f"Running a predicate of type '{pred_type}' with mapping: {mapping}")
+
+            if pred_type == "once":
+                # Evaluate the predicate on the current state. By construction, "mapping" will 
+                # include the assignment for all arguments needed by this particular predicate
+                pred_eval = self.evaluate_predicate(current_predicate["once_pred"]["pred"], traj_state, mapping)
+                print("Overall evaluation:", pred_eval)
+
+
+
+    def evaluate_predicate(self, predicate, state, mapping):
+        '''
+        Given a predicate, a trajectory state, and an assignment of each of the predicate's
+        arguments to specific objects in the state, returns the evaluation of the predicate
+        '''
+        for key in predicate:
+            if key == "pred_name":
+                # Obtain the functional representation of the base predicate
+                predicate_fn = PREDICATE_LIBRARY[predicate["pred_name"]]
+
+                # Map the variables names to the object names, and extract them from the state
+                predicate_args = [state["objects"][mapping[var]] for var in self._extract_variables(predicate)]
+                
+                # Evaluate the predicate
+                evaluation = predicate_fn(state, *predicate_args)
+
+                # print(f"Evaluation of predicate '{predicate['pred_name']}' with args = {predicate_args}: {evaluation}")
+                return evaluation
+
+            elif key == "not_args":
+                return not self.evaluate_predicate(predicate[key]["pred"], state, mapping)
+
+            elif key == "and_args":
+                return all([self.evaluate_predicate(sub["pred"], state, mapping) for sub in predicate[key]])
+
+            elif key == "or_args":
+                return any([self.evaluate_predicate(sub["pred"], state, mapping) for sub in predicate[key]])
 
 
 if __name__ == "__main__":
@@ -128,6 +165,8 @@ if __name__ == "__main__":
         (preference throwBallToBin
             (exists (?d - ball ?h - bin)
                 (then
+                    (once (and (agent_holds ?d) (in_motion ?d)))
+                    (once (or (in ?d ?h) (and (in_motion ?d) (not (agent_holds ?d))) (agent_crouches)))
                     (once (or (agent_holds ?d) (and (in_motion ?h) (< (distance agent ?d) 5)) ))
                     (hold-while (and (not (agent_holds ?d)) (in_motion ?d)) (in_motion ?h) (agent_crouches))
                     (once-measure (and (not (in_motion ?d)) (in ?h ?d)) (color ?h))
@@ -150,10 +189,37 @@ if __name__ == "__main__":
     )))
     """
 
+    DUMMY_STATE = {"objects": {"blue-dodgeball-1": {"name": "blue-dodgeball-1", "position": [4, 0, 0],
+                                                    "velocity": [0, 0, 0], "objectType": "ball", 
+                                                    "color": "blue"},
+
+                               "pink-dodgeball-1": {"name": "pink-dodgeball-1", "position": [0, 4, 0],
+                                                    "velocity": [0, 1, 0], "objectType": "ball",
+                                                    "color": "pink"},
+
+                               "red-dodgeball-1": {"name": "red-dodgeball-1", "position": [4, 4, 0],
+                                                    "velocity": [1, 0, 0], "objectType": "ball",
+                                                    "color": "red"},
+
+                               "hexagonal-bin-1": {"name": "hexagonal-bin-1", "position": [15, 10, 0],
+                                                    "velocity": [0, 0, 0], "objectType": "bin"},
+
+                               "hexagonal-bin-2": {"name": "hexagonal-bin-2", "position": [10, 15, 0],
+                                                    "velocity": [0, 0, 0], "objectType": "bin"},
+
+                               "agent": {"name": "agent", "position": [0, 0, 0], "velocity": [0, 0, 0],
+                                         "is_crouching": False, "holding": "red-dodgeball-1", "objectType": "agent"},
+                              },
+
+                   "game_start": True,
+                   "game_over": False
+
+                  }
+
     ast = grammar_parser.parse(test_game)
 
     preferences = ast[3][1]["preferences"]
     
     pref1 = preferences[0]
     handler1 = PreferenceHandler(pref1)
-    handler1.process(None)
+    handler1.process(DUMMY_STATE)

9e23a328736771138d621551b7438fcae479e8ac
Guy Davidson <guydav@gmail.com>
2022-11-01 16:41:36 -0400

    DSL fixes + doc cleanup and bugfixes

--- src/ast_to_latex_doc.py
+++ src/ast_to_latex_doc.py
@@ -2,6 +2,7 @@ import argparse
 from collections import defaultdict, namedtuple
 from itertools import chain
 import tatsu
+import tatsu.ast
 import shutil
 import os
 
@@ -501,7 +502,7 @@ SHARED_BLOCKS = {
     PREDICATE: (r"""<predicate> ::= (<name> <predicate-term>$^*$)
 
 <predicate-term> ::= <name> | <variable>""",
-('predicate',)),
+('predicate', 'predicate_term')),
 }
 
 
@@ -683,7 +684,6 @@ PREDICATE_DESCRIPTIONS = {
     'same_type': 'Are these two objects of the same type? Or if one is a direct reference to a type, is this object of that type?',
     'toggled_on': 'Is this object toggled on?',
     'touch': 'Are these two objects touching?',
-    'type': 'Is the first argument, an object, an instance of the type specified by the second argument?',    
 }
 
 FUNCTION_DESCRIPTIONS = {
@@ -691,7 +691,6 @@ FUNCTION_DESCRIPTIONS = {
     'color': 'Take in an argument of type object, and returns the color of the object (as a color type object).',
     'distance': 'Takes in two arguments of type object, and returns the distance between the two objects (as a floating point number).',
     'distance_side': 'Similarly to the adjacent_side predicate, but applied to distance. Takes in three or four arguments, either <obj1> <side1> <obj2> or <obj1> <side1> <obj2> <side2>, and returns the distance between the first object on the side specified to the second object (optionally to its specified side).',
-    'type': 'Takes in an argument of type object, and returns the type of the object (as a string).',
     'x_position': 'Takes in an argument of type object, and returns the x position of the object (as a floating point number).',
 }
 
@@ -815,8 +814,8 @@ FUNCTION_RULES = {
 
 
 def extract_single_variable_type(ast):
-    if 'var_type' in ast and isinstance(ast.var_type, str):
-        return ast.var_type
+    if 'var_type' in ast and 'type' in ast.var_type and isinstance(ast.var_type.type, str):
+        return ast.var_type.type
 
     return None
 
@@ -841,16 +840,23 @@ def extract_pref_name_and_types(ast):
     return None
 
 
-def extract_types_from_predicates(ast):
+def extract_types_from_predicates_and_functions(ast):
+    args = None
+    
     if 'pred_args' in ast:
-        if isinstance(ast.pred_args, str) and not ast.pred_args.startswith('?'):
-            return ast.pred_args
+        args = ast.pred_args
+    
+    elif 'func_args' in ast:
+        args = ast.func_args
 
-        filtered_args = [arg for arg in ast.pred_args if isinstance(arg, str) and not arg.startswith('?')]
-        if filtered_args:
-            return filtered_args
+    if args is None:
+        return None
 
-    return None
+    if isinstance(args, tatsu.ast.AST):
+        args = [args]
+
+    return [arg.term for arg in args if 'term' in arg and isinstance(arg.term, str) and not arg.term.startswith('?')]
+
 
 
 def extract_co_ocurring_types(ast, key):
@@ -869,7 +875,8 @@ TYPE_RULES = {
     'variable_type_def': (extract_single_variable_type, None),
     'either_types': (extract_either_variable_types, None),
     'pref_name_and_types': (extract_pref_name_and_types, None),
-    'predicate': (extract_types_from_predicates, None),
+    'predicate': (extract_types_from_predicates_and_functions, None),
+    'function': (extract_types_from_predicates_and_functions, None),
 }
 
 SCORING_CONSIDER_USED_RULES = (

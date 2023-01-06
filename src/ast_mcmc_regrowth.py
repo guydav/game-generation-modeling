f3c095cbe5fd7b124cc7f39351db6fcc07326f5e
Guy Davidson <guydav@gmail.com>
2023-01-06 11:52:55 -0500

    Minor code cleanup around the MCMC sampler

--- src/ast_mcmc_regrowth.py
+++ src/ast_mcmc_regrowth.py
@@ -52,27 +52,27 @@ class MCMCRegrowthSampler:
         self.sample_index = 0
         self.samples = []
 
-    def multiple_samples(self, n_samples: int, should_tqdm: bool = False, verbose: bool = False, should_print: bool = False):
-        sample_iter = tqdm.trange(n_samples) if should_tqdm else range(n_samples)
+    def multiple_samples(self, n_samples: int, verbose: int = 0, should_tqdm: bool = False):
+        sample_iter = tqdm.notebook.trange(n_samples) if should_tqdm else range(n_samples)
         for _ in sample_iter:
-            self.sample(verbose=verbose, should_print=should_print)
+            self.sample(verbose)
 
-    def sample(self, verbose: bool = False, should_print: bool = False):
+    def sample(self, verbose: int = 0):
         current_proposal = None
         while current_proposal is None:
             try:
                 current_proposal = self.sampler.sample(global_context=dict(original_game_id=f'mcmc-{self.sample_index}'))
             except RecursionError:
-                print('Recursion error, skipping sample')
+                if verbose >= 2: print('Recursion error, skipping sample')
             except SamplingException:
-                print('Sampling exception, skipping sample')
-                
-        current_proposal_fitness = self._score_proposal(current_proposal)  # type: ignore
+                if verbose >= 2: print('Sampling exception, skipping sample')
+            
+        current_proposal_features, current_proposal_fitness = self._score_proposal(current_proposal)  # type: ignore
 
         last_accepted_step = 0
         for step in range(self.max_steps):
-            current_proposal, current_proposal_fitness, accepted = self.mcmc_regrowth_step(
-                current_proposal, current_proposal_fitness, step  # type: ignore
+            current_proposal, current_proposal_features, current_proposal_fitness, accepted = self.mcmc_regrowth_step(
+                current_proposal, current_proposal_features, current_proposal_fitness, step, verbose  # type: ignore
             )
 
             if accepted:
@@ -86,18 +86,14 @@ class MCMCRegrowthSampler:
                         print(f'Plateaued at step {step} with fitness {current_proposal_fitness}')
                     break
 
-        if should_print:
-            print(f'Sample #{self.sample_index} with fitness {current_proposal_fitness:.4f}:')
-            print(ast_printer.ast_to_string(current_proposal, line_delimiter='\n'))
-            print('=' * 120)
-
-        self.samples.append((current_proposal, current_proposal_fitness))
+        self.samples.append((current_proposal, current_proposal_features, current_proposal_fitness))
         self.sample_index += 1
 
     def mcmc_regrowth_step(self,
         current_proposal: tatsu.ast.AST, 
+        current_proposal_features: typing.Dict[str, float],
         current_proposal_fitness: float,
-        step_index: int,
+        step_index: int, verbose: int,
         ):
 
         if self.regrowth_sampler.source_ast != current_proposal:
@@ -105,48 +101,50 @@ class MCMCRegrowthSampler:
 
         new_proposal = None
         sample_generated = False
+
         while not sample_generated:
             try:
-                new_proposal = self.regrowth_sampler.sample(step_index)
+                new_proposal = self.regrowth_sampler.sample(step_index, update_game_id=False)
                 # _test_ast_sample(ast, args, text_samples, grammar_parser)
                 if ast_printer.ast_to_string(new_proposal) == ast_printer.ast_to_string(current_proposal):  # type: ignore
-                    print('Regrowth generated identical games, repeating')
+                    if verbose >= 2: print('Regrowth generated identical games, repeating')
                 else:
                     sample_generated = True
+
             except RecursionError:
-                print('Recursion error, skipping sample')
+                if verbose >= 2: print('Recursion error, skipping sample')
 
             except SamplingException:
-                print('Sampling exception, skipping sample')
+                if verbose >= 2: print('Sampling exception, skipping sample')
 
-        new_proposal_fitness = self._score_proposal(new_proposal)  # type: ignore
+        new_proposal_features, new_proposal_fitness = self._score_proposal(new_proposal)  # type: ignore
         
         if self.greedy_acceptance:
-            if new_proposal_fitness < current_proposal_fitness:
-                return new_proposal, new_proposal_fitness, True
-            else:
-                return current_proposal, current_proposal_fitness, False
-
+            accept = new_proposal_fitness < current_proposal_fitness
         else:
             acceptance_probability = np.exp(-self.acceptance_temperature * (new_proposal_fitness - current_proposal_fitness))
-            if self.rng.uniform() < acceptance_probability:
-                return new_proposal, new_proposal_fitness, True
-            else:
-                return current_proposal, current_proposal_fitness, False
+            accept = self.rng.uniform() < acceptance_probability
+
+        if accept:
+            return new_proposal, new_proposal_features, new_proposal_fitness, True
+        else:
+            return current_proposal, current_proposal_features, current_proposal_fitness, False
 
     def _score_proposal(self, proposal: tatsu.ast.AST):
-        proposal_features = self.fitness_featurizer.parse(proposal, 'mcmc', True)  # type: ignore
-        proposal_tensor = torch.tensor([v for k, v in proposal_features.items() if k not in NON_FEATURE_COLUMNS])  # type: ignore
+        proposal_features = typing.Cast(dict, self.fitness_featurizer.parse(proposal, 'mcmc', True))  # type: ignore
+        proposal_tensor = torch.tensor([v for k, v in proposal_features.items() if k not in NON_FEATURE_COLUMNS], 
+            dtype=torch.float32)  # type: ignore
         proposal_fitness = self.fitness_function(proposal_tensor)
-        return proposal_fitness
+        return proposal_features, proposal_fitness
 
 
 def main(args: argparse.Namespace):
     original_recursion_limit = sys.getrecursionlimit()
     sys.setrecursionlimit(args.recursion_limit)
 
-    mcmc = MCMCRegrowthSampler(args, DEFAULT_FITNESS_FUNCTION_PATH, greedy_acceptance=True, plateau_patience_steps=30)
-    mcmc.multiple_samples(args.num_samples, verbose=args.verbose, should_tqdm=args.sample_tqdm, should_print=args.print_samples)
+    mcmc = MCMCRegrowthSampler(args, DEFAULT_FITNESS_FUNCTION_PATH, greedy_acceptance=False, 
+        plateau_patience_steps=50, acceptance_temperature=10.0, max_steps=3000)
+    mcmc.multiple_samples(args.num_samples, verbose=args.verbose, should_tqdm=args.sample_tqdm)
 
     if args.save_samples:
         samples = mcmc.samples

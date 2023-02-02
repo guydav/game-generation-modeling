8bb37c4332f22aeb5d69f1c8cc3e3983b95f0ec6
Guy Davidson <guydav@gmail.com>
2023-02-02 11:42:19 -0500

    Implemented fitness features on a per-row basis
    Assumes the featurizer was called on an entire dataframe first to 'fit' it.

--- src/fitness_features_preprocessing.py
+++ src/fitness_features_preprocessing.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from bisect import bisect
 from functools import reduce
 import re
@@ -7,7 +8,16 @@ import numpy as np
 import pandas as pd
 
 
-## -- Binarization and other preprocessing -- ##
+
+class FitnessFeaturesPreprocessor(ABC):
+    @abstractmethod
+    def preprocess_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+
+    @abstractmethod
+    def preprocess_row(self, row: typing.Dict[str, typing.Any]) -> typing.Dict[str, typing.Any]:
+        pass
 
 
 NON_FEATURE_COLUMNS = set(['Index', 'src_file', 'game_name', 'domain_name', 'real', 'original_game_name'])
@@ -43,34 +53,54 @@ BINRARIZE_NONZERO_PATTERNS = [
     re.compile(r'(ast|text)_ngram_n_\d+_\d+')
 ]
 
+class BinarizeFitnessFeatures(FitnessFeaturesPreprocessor):
+    ignore_columns: typing.Iterable[str]
+    scale_series_min_max_values: typing.Dict[str, typing.Tuple[float, float]]
 
-def _binarize_series(series: pd.Series, ignore_columns: typing.Iterable[str] = NON_FEATURE_COLUMNS):
-    c = str(series.name)
-    if c in ignore_columns:
-        return series
+    def __init__(self, ignore_columns: typing.Iterable[str] = NON_FEATURE_COLUMNS):
+        self.ignore_columns = ignore_columns
+        self.scale_series_min_max_values = {}
 
-    if c in BINARIZE_IGNORE_FEATURES:
-        return series
+    def _binarize_series(self, series: pd.Series, ignore_columns: typing.Iterable[str] = NON_FEATURE_COLUMNS):
+        c = str(series.name)
+        if c in ignore_columns:
+            return series
 
-    if any([p.match(c) for p in BINARIZE_IGNORE_PATTERNS]):
-        return series
+        if c in BINARIZE_IGNORE_FEATURES:
+            return series
 
-    if c in BINARIZE_NON_ONE:
-        return (series == 1).astype(int)
+        if any([p.match(c) for p in BINARIZE_IGNORE_PATTERNS]):
+            return series
 
-    if any([p.match(c) for p in SCALE_ZERO_ONE_PATTERNS]):
-        min_val, max_val = series.min(), series.max()
-        return (series - min_val) / (max_val - min_val)
+        if c in BINARIZE_NON_ONE:
+            return (series == 1).astype(int)
 
-    if any([p.match(c) for p in BINRARIZE_NONZERO_PATTERNS]):
-        return (series != 0).astype(int)
+        if any([p.match(c) for p in SCALE_ZERO_ONE_PATTERNS]):
+            min_val, max_val = series.min(), series.max()
+            self.scale_series_min_max_values[c] = (min_val, max_val)
+            return (series - min_val) / (max_val - min_val)
 
-    raise ValueError(f'No binarization rule for column {c}')
+        if any([p.match(c) for p in BINRARIZE_NONZERO_PATTERNS]):
+            return (series != 0).astype(int)
 
+        raise ValueError(f'No binarization rule for column {c}')
 
-def binarize_features(df: pd.DataFrame, ignore_columns: typing.Iterable[str] = NON_FEATURE_COLUMNS) -> pd.DataFrame:
-    binarized_df = df.apply(_binarize_series, axis=0, ignore_columns=ignore_columns)
-    return binarized_df
+    def preprocess_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.apply(self._binarize_series, axis=0, ignore_columns=self.ignore_columns)
+
+    def preprocess_row(self, row: typing.Dict[str, typing.Any]) -> typing.Dict[str, typing.Any]:
+        for k, v in row.items():
+            if k in BINARIZE_NON_ONE:
+                row[k] = 1 if v == 1 else 0
+
+            if k in self.scale_series_min_max_values:
+                min_val, max_val = self.scale_series_min_max_values[k]
+                row[k] = (v - min_val) / (max_val - min_val)
+
+            if any([p.match(k) for p in BINRARIZE_NONZERO_PATTERNS]):
+                row[k] = 1 if v != 0 else 0
+
+        return row
 
 
 DEFAULT_MERGE_THRESHOLD = 10
@@ -78,54 +108,135 @@ DEFAULT_FEATURE_SUFFIXES = ('setup', 'constraints')
 DEFAULT_MERGE_COLUMN_SUFFIX = 'other'
 
 
-def _merge_single_prefix(df: pd.DataFrame, feature_prefix: str, threshold: int = DEFAULT_MERGE_THRESHOLD,
-    merge_function: typing.Callable = np.logical_or, merged_column_suffix: str = DEFAULT_MERGE_COLUMN_SUFFIX,
-    feature_suffix: str = '') -> None:
+# def _merge_single_prefix(df: pd.DataFrame, feature_prefix: str, threshold: int = DEFAULT_MERGE_THRESHOLD,
+#     merge_function: typing.Callable = np.logical_or, merged_column_suffix: str = DEFAULT_MERGE_COLUMN_SUFFIX,
+#     feature_suffix: str = '') -> None:
 
-    merged_column_key = f'{feature_prefix}_{merged_column_suffix}{"_" + feature_suffix if feature_suffix else ""}'
-    prefix_feature_names = [c for c in df.columns if c.startswith(feature_prefix) and c.endswith(feature_suffix)]
-    if len(prefix_feature_names) == 0:
-        raise ValueError(f'No features found for prefix {feature_prefix} and suffix {feature_suffix}')
-    merged_column_insert_index = bisect(prefix_feature_names, merged_column_key)
-    first_prefix_feature_index = list(df.columns).index(prefix_feature_names[0])
-    insert_index = first_prefix_feature_index + merged_column_insert_index
+#     merged_column_key = f'{feature_prefix}_{merged_column_suffix}{"_" + feature_suffix if feature_suffix else ""}'
+#     prefix_feature_names = [c for c in df.columns if c.startswith(feature_prefix) and c.endswith(feature_suffix)]
+#     if len(prefix_feature_names) == 0:
+#         raise ValueError(f'No features found for prefix {feature_prefix} and suffix {feature_suffix}')
+#     merged_column_insert_index = bisect(prefix_feature_names, merged_column_key)
+#     first_prefix_feature_index = list(df.columns).index(prefix_feature_names[0])
+#     insert_index = first_prefix_feature_index + merged_column_insert_index
 
-    counts = df[[c for c in df.columns if c.startswith(feature_prefix) and c.endswith(feature_suffix)]].sum()
-    keys_to_merge = counts.index[counts < threshold]  # type: ignore
-    if len(keys_to_merge) == 0:
-        print(feature_prefix)
-        return
-    new_series_values = reduce(merge_function, [df[k] for k in keys_to_merge[1:]], df[keys_to_merge[0]]).astype(int)
+#     counts = df[[c for c in df.columns if c.startswith(feature_prefix) and c.endswith(feature_suffix)]].sum()
+#     keys_to_merge = counts.index[counts < threshold]  # type: ignore
+#     if len(keys_to_merge) == 0:
+#         print(feature_prefix)
+#         return
+#     new_series_values = reduce(merge_function, [df[k] for k in keys_to_merge[1:]], df[keys_to_merge[0]]).astype(int)
 
-    df.insert(insert_index, merged_column_key, new_series_values)
-    df.drop(keys_to_merge, axis=1, inplace=True)
+#     df.insert(insert_index, merged_column_key, new_series_values)
+#     df.drop(keys_to_merge, axis=1, inplace=True)
 
 
-def merge_sparse_features(df: pd.DataFrame, predicates: typing.Sequence[str],
-    threshold: int = DEFAULT_MERGE_THRESHOLD, merge_function: typing.Callable = np.logical_or,
-    merged_column_suffix: str = 'other', feature_suffixes: typing.Sequence[str] = DEFAULT_FEATURE_SUFFIXES
-    ) -> pd.DataFrame:
+# def merge_sparse_features(df: pd.DataFrame, predicates: typing.Sequence[str],
+#     threshold: int = DEFAULT_MERGE_THRESHOLD, merge_function: typing.Callable = np.logical_or,
+#     merged_column_suffix: str = DEFAULT_MERGE_COLUMN_SUFFIX, feature_suffixes: typing.Sequence[str] = DEFAULT_FEATURE_SUFFIXES
+#     ) -> pd.DataFrame:
 
-    df = df.copy(deep=True)
+#     df = df.copy(deep=True)
 
-    for feature_suffix in feature_suffixes:
-        for p in predicates:
-            feature_prefix = f'{p}_arg_types'
-            _merge_single_prefix(df, feature_prefix, threshold, merge_function, merged_column_suffix, feature_suffix)
+#     for feature_suffix in feature_suffixes:
+#         for p in predicates:
+#             feature_prefix = f'{p}_arg_types'
+#             _merge_single_prefix(df, feature_prefix, threshold, merge_function, merged_column_suffix, feature_suffix)
 
-            # if p not in PREDICATE_FUNCTION_ARITY_MAP:
-            #     raise ValueError(f'Predicate {p} not in arity map')
+#             # if p not in PREDICATE_FUNCTION_ARITY_MAP:
+#             #     raise ValueError(f'Predicate {p} not in arity map')
 
-            # arity = PREDICATE_FUNCTION_ARITY_MAP[p]
-            # if arity == 1:
-            #     feature_prefix = f'arg_types_{p}'
-            #     _merge_single_prefix(df, feature_prefix, threshold, merge_function, merged_column_suffix, feature_suffix)
+#             # arity = PREDICATE_FUNCTION_ARITY_MAP[p]
+#             # if arity == 1:
+#             #     feature_prefix = f'arg_types_{p}'
+#             #     _merge_single_prefix(df, feature_prefix, threshold, merge_function, merged_column_suffix, feature_suffix)
 
-            # else:  # arity = 2/3
-            #     for c in CATEGORIES_TO_TYPES.keys():
-            #         if c == EMPTY_OBJECT:
-            #             continue
-            #         feature_prefix = f'arg_types_{p}_{c}'
-            #         _merge_single_prefix(df, feature_prefix, threshold, merge_function, merged_column_suffix, feature_suffix)
+#             # else:  # arity = 2/3
+#             #     for c in CATEGORIES_TO_TYPES.keys():
+#             #         if c == EMPTY_OBJECT:
+#             #             continue
+#             #         feature_prefix = f'arg_types_{p}_{c}'
+#             #         _merge_single_prefix(df, feature_prefix, threshold, merge_function, merged_column_suffix, feature_suffix)
 
-    return df
+    # return df
+
+
+class MergeFitnessFeatures(FitnessFeaturesPreprocessor):
+    feature_suffixes: typing.Sequence[str]
+    keys_to_drop: typing.List[str]
+    merge_function: typing.Callable
+    merged_column_suffix: str
+    merged_key_indices: typing.Dict[str, int]
+    merged_key_to_original_keys: typing.Dict[str, typing.List[str]]
+    predicates: typing.Sequence[str]
+    threshold: int
+
+    def __init__(self, predicates: typing.Sequence[str], threshold: int = DEFAULT_MERGE_THRESHOLD,
+                 merge_function: typing.Callable = np.logical_or, merged_column_suffix: str = DEFAULT_MERGE_COLUMN_SUFFIX,
+                 feature_suffixes: typing.Sequence[str] = DEFAULT_FEATURE_SUFFIXES):
+
+        self.predicates = predicates
+        self.threshold = threshold
+        self.merge_function = merge_function
+        self.merged_column_suffix = merged_column_suffix
+        self.feature_suffixes = feature_suffixes
+
+        self.keys_to_drop = []
+        self.merged_key_indices = {}
+        self.merged_key_to_original_keys = {}
+
+    def _merge_single_prefix(self, df: pd.DataFrame, feature_prefix: str, feature_suffix: str = '') -> None:
+
+        merged_column_key = f'{feature_prefix}_{self.merged_column_suffix}{"_" + feature_suffix if feature_suffix else ""}'
+        prefix_feature_names = [c for c in df.columns if c.startswith(feature_prefix) and c.endswith(feature_suffix)]
+        if len(prefix_feature_names) == 0:
+            raise ValueError(f'No features found for prefix {feature_prefix} and suffix {feature_suffix}')
+        merged_column_insert_index = bisect(prefix_feature_names, merged_column_key)
+        first_prefix_feature_index = list(df.columns).index(prefix_feature_names[0])
+        insert_index = first_prefix_feature_index + merged_column_insert_index
+        self.merged_key_indices[merged_column_key] = insert_index
+
+        counts = df[[c for c in df.columns if c.startswith(feature_prefix) and c.endswith(feature_suffix)]].sum()
+        keys_to_merge = counts.index[counts < threshold]  # type: ignore
+        if len(keys_to_merge) == 0:
+            print(feature_prefix)
+            return
+
+        new_series_values = reduce(self.merge_function, [df[k] for k in keys_to_merge[1:]], df[keys_to_merge[0]]).astype(int)
+        self.merged_key_to_original_keys[merged_column_key] = list(keys_to_merge)
+
+        df.insert(insert_index, merged_column_key, new_series_values)
+        self.keys_to_drop.extend(keys_to_merge)
+
+    def preprocess_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.copy(deep=True)
+
+        for feature_suffix in self.feature_suffixes:
+            for p in self.predicates:
+                feature_prefix = f'{p}_arg_types'
+                self._merge_single_prefix(df, feature_prefix, feature_suffix)
+
+        df.drop(self.keys_to_drop, axis=1, inplace=True)
+
+        return df
+
+    def preprocess_row(self, row: typing.Dict[str, typing.Any]) -> typing.Dict[str, typing.Any]:
+        # TODO: this might be slow because of the number of inserts, consider optimizing further if it proves annoying
+        keys = list(row.keys())
+        merged_key_values = {}
+
+        for merged_key in self.merged_key_indices:
+            insert_index = self.merged_key_indices[merged_key]
+            keys.insert(insert_index, merged_key)
+            merged_key_values[merged_key] = reduce(self.merge_function, [row[k] for k in self.merged_key_to_original_keys[merged_key]], row[self.merged_key_to_original_keys[merged_key][0]])
+
+        new_row = {}
+        for k in keys:
+            if k in self.keys_to_drop:
+                continue
+            if k in merged_key_values:
+                new_row[k] = merged_key_values[k]
+            else:
+                new_row[k] = row[k]
+
+        return new_row

c0e3b606bc41b87df601bd286d29e56c8eb1fc5d
Graham Todd <gdrtodd@gmail.com>
2023-02-27 15:00:18 -0500

    Add option for ngram based fitness function

--- src/evolutionary_sampler.py
+++ src/evolutionary_sampler.py
@@ -1,3 +1,23 @@
+import argparse
+import gzip
+import os
+import pickle
+import typing
+import sys
+
+import numpy as np
+import tqdm
+import tatsu
+import tatsu.ast
+import torch
+
+import ast_printer
+from ast_counter_sampler import *
+from ast_counter_sampler import parse_or_load_counter, ASTSampler, RegrowthSampler, SamplingException, MCMC_REGRWOTH
+from ast_crossover_sampler import CrossoverSampler, CrossoverType
+from ast_utils import *
+
+from fitness_ngram_models import NGramTrieModel, NGramTrieNode, NGramASTParser, ASTNGramTrieModel
 
 class EvolutionarySampler():
     '''
@@ -5,8 +25,13 @@ class EvolutionarySampler():
     provided fitness function. It's a population-based alternative to the MCMC samper
     '''
 
-    def __init__(self, etc):
+    def __init__(self,
+                 args: argparse.Namespace,
+                 fitness_function: typing.Callable[[typing.Any], float],
+                 population_size: int = 100,
+                 verbose: int = 0):
 
+        self.population_size = population_size
         self.verbose = verbose
 
         self.grammar = open(args.grammar_file).read()
@@ -17,10 +42,10 @@ class EvolutionarySampler():
         self.initial_sampler = ASTSampler(self.grammar_parser, self.counter, seed=args.random_seed)
         
         # Used as the mutation operator to modify existing games
-        self.regrowth_sampler = RegrowthSampler(self.sampler, args.random_seed)
+        self.regrowth_sampler = RegrowthSampler(self.initial_sampler, args.random_seed)
 
         # Generate the initial population
-        self.population = [self._init_sample(idx) for idx in range(args.population_size)]
+        self.population = [self._init_sample(idx) for idx in range(self.population_size)]
 
     def _init_sample(self, idx):
         '''
@@ -32,7 +57,7 @@ class EvolutionarySampler():
 
         while sample is None:
             try:
-                sample = typing.cast(tuple, self.sampler.sample(global_context=dict(original_game_id=f'mcmc-{idx}')))
+                sample = typing.cast(tuple, self.initial_sampler.sample(global_context=dict(original_game_id=f'mcmc-{idx}')))
             except RecursionError:
                 if self.verbose >= 2: print(f'Recursion error in sample {idx} -- skipping')
             except SamplingException:
@@ -42,3 +67,33 @@ class EvolutionarySampler():
 
     def step(self):
         pass
+
+if __name__ == '__main__':
+
+    DEFAULT_GRAMMAR_FILE = './dsl/dsl.ebnf'
+    DEFAULT_COUNTER_OUTPUT_PATH ='./data/ast_counter.pickle'
+    DEFUALT_RANDOM_SEED = 0
+    # DEFAULT_NGRAM_MODEL_PATH = '../models/text_5_ngram_model_2023_02_17.pkl'
+    DEFAULT_NGRAM_MODEL_PATH = '../models/ast_7_ngram_model_2023_02_16.pkl'
+
+    DEFAULT_ARGS = argparse.Namespace(
+        grammar_file=os.path.join('..', DEFAULT_GRAMMAR_FILE),
+        parse_counter=False,
+        counter_output_path=os.path.join('..', DEFAULT_COUNTER_OUTPUT_PATH),
+        random_seed=DEFUALT_RANDOM_SEED,
+    )
+
+    with open(DEFAULT_NGRAM_MODEL_PATH, 'rb') as file:
+        ngram_model = pickle.load(file)
+
+    def fitness_function(game):
+        '''
+        A simple wrapper around the ASTNGramTrieModel.score function, allowing it
+        to run on just a single game and returning the score as float
+        '''
+        return ngram_model.score(game, k=None, score_all=False)['score']
+
+    evosampler = EvolutionarySampler(DEFAULT_ARGS, fitness_function, verbose=0)
+
+    for i in range(len(evosampler.population)):
+        print(fitness_function(evosampler.population[i]))

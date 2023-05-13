1c1d923861970005ed526fbb139de66a2524f45b
guydav <gd1279@nyu.edu>
2023-05-13 00:48:19 -0400

    Latest energy fitness model

--- src/latest_model_paths.py
+++ src/latest_model_paths.py
@@ -5,5 +5,5 @@ _FILE_DIR = os.path.dirname(__file__)
 
 LATEST_AST_N_GRAM_MODEL_PATH = os.path.abspath(os.path.join(_FILE_DIR, '../models/ast_7_ngram_model_2023_05_09.pkl'))
 LATEST_FITNESS_FEATURIZER_PATH = os.path.abspath(os.path.join(_FILE_DIR, '../models/fitness_featurizer_2023_05_12.pkl.gz'))
-LATEST_FITNESS_FUNCTION_DATE_ID = 'full_features_2023_05_09'
+LATEST_FITNESS_FUNCTION_DATE_ID = 'full_features_2023_05_12'
 LATEST_FITNESS_FEATURES =  os.path.abspath(os.path.join(_FILE_DIR,  '../data/fitness_features_1024_regrowths.csv.gz'))

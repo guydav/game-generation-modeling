c98e0e767a68aeb294a1244c685fd8f4e857f70a
Guy Davidson <guydav@gmail.com>
2023-06-03 21:24:29 -0400

    WIP on PCA-based featurizer + length prior for then.then_funcs

--- src/latest_model_paths.py
+++ src/latest_model_paths.py
@@ -3,6 +3,8 @@ import os
 
 _FILE_DIR = os.path.dirname(__file__)
 
+
+LATEST_REAL_GAMES_PATH = os.path.abspath(os.path.join(_FILE_DIR, '../dsl/interactive-beta.pddl'))
 LATEST_AST_N_GRAM_MODEL_PATH = os.path.abspath(os.path.join(_FILE_DIR, '../models/ast_7_ngram_model_2023_05_22.pkl'))
 LATEST_FITNESS_FEATURIZER_PATH = os.path.abspath(os.path.join(_FILE_DIR, '../models/fitness_featurizer_2023_05_24.pkl.gz'))
 LATEST_FITNESS_FUNCTION_DATE_ID = 'full_features_2023_05_24'

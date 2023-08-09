0ff2e472ab5c84b3389006ab394c12c42d0259cb
Guy Davidson <guydav@gmail.com>
2023-08-09 21:38:01 +0300

    Restored memory tracing in test script

--- notebooks/test.py
+++ notebooks/test.py
@@ -21,7 +21,7 @@ from src.fitness_features import *
 
 
 PROFILE = False
-MEMORY_TRACE = False
+MEMORY_TRACE = True
 
 
 if __name__ == '__main__':
@@ -29,8 +29,8 @@ if __name__ == '__main__':
     grammar_parser = tatsu.compile(grammar)
     game_asts = list(cached_load_and_parse_games_from_file('./dsl/interactive-beta.pddl', grammar_parser, False, relative_path='.'))
 
-    # if MEMORY_TRACE:
-    #     tracemalloc.start()
+    if MEMORY_TRACE:
+        tracemalloc.start()
 
     args = Namespace(no_binarize=False, no_merge=False, use_specific_objects_ngram_model=False)
     featurizer = build_fitness_featurizer(args)
@@ -44,9 +44,9 @@ if __name__ == '__main__':
 
     if MEMORY_TRACE:
         tracemalloc.start(25)
-    #     size, peak = tracemalloc.get_traced_memory()
-    #     print(f'After creating featurizer | Memory usage is {size / 10**6}MB | Peak was {peak / 10**6}MB')
-    #     tracemalloc.reset_peak()
+        size, peak = tracemalloc.get_traced_memory()
+        print(f'After creating featurizer | Memory usage is {size / 10**6}MB | Peak was {peak / 10**6}MB')
+        tracemalloc.reset_peak()
 
     start = time.perf_counter()
     for i in range(0, len(game_asts)):
@@ -57,12 +57,12 @@ if __name__ == '__main__':
         # print(f'Parsing game #{i}: {game_asts[i][1].game_name}')
         featurizer.parse(game_asts[i], 'interactive-beta.pddl')
 
-        if MEMORY_TRACE:
-            # gc.collect()
-            size, peak = tracemalloc.get_traced_memory()
-            print(f'After current game | Memory usage is {size / 10**6}MB | Peak was {peak / 10**6}MB')
-            tracemalloc.reset_peak()
-            print()
+        # if MEMORY_TRACE:
+        #     # gc.collect()
+        #     size, peak = tracemalloc.get_traced_memory()
+        #     print(f'After current game | Memory usage is {size / 10**6}MB | Peak was {peak / 10**6}MB')
+        #     tracemalloc.reset_peak()
+        #     print()
 
         # second_snapshot = tracemalloc.take_snapshot()
         # top_stats = second_snapshot.compare_to(first_snapshot, 'lineno')

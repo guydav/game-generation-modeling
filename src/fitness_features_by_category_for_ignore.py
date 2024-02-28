9f5cf8ee672d69b74dc8121fb0ed7f6b581a6d9b
guydav <gd1279@nyu.edu>
2024-02-28 15:24:45 -0500

    Updated fitness function features to be allow-list based, rather than which to ignore

--- src/fitness_features_by_category_for_ignore.py
+++ src/fitness_features_by_category_for_ignore.py
@@ -4,122 +4,59 @@ import re
 from fitness_features import COMMON_SENSE_PREDICATES_FUNCTIONS
 from fitness_features_preprocessing import NGRAM_SCORE_PATTERN, ARG_TYPES_PATTERN
 
-
-NGRAM_FEATURES = [
-    NGRAM_SCORE_PATTERN,
-]
-
-
-PLAY_TRACE_DATABASE_FEATURES = [
-    re.compile(r'predicate_found_in_data_[\w\d_]+'),
-]
-
-
-DEFINED_AND_USED_FEATURES = [
-    # did you use the things you defined
+GRAMMAR_CONTEXT_FEATURES = [
     'variables_used_all',
     'variables_used_prop',
     'preferences_used_all',
     'preferences_used_prop',
-    'setup_quantified_objects_used',
-    'any_setup_objects_used',
-    # indicators for section existence
+
+    'repeated_variables_found',
+    'repeated_variable_type_in_either',
+
     'section_doesnt_exist_setup',
     'section_doesnt_exist_terminal',
 ]
 
-
-GRAMMAR_MISUSE_FEATURES = [
-    # modals
-    'adjacent_once_found',
-    'adjacent_same_modal_found',
-    'once_in_middle_of_pref_found',
-    'pref_without_hold_found',
-    # predicates?
-    'identical_consecutive_seq_func_predicates_found',
-    'predicate_without_variables_or_agent',
-    # logical/scoring expressions
-    'nested_logicals_found',
-    'identical_logical_children_found',
-    'redundant_expression_found',
-    'unnecessary_expression_found',
-    # repeatedness -- in predicates, in quantifications
-    'repeated_variables_found',
-    'repeated_variable_type_in_either',
+NGRAM_AND_DATA_BASED_FEATURES = [
+    NGRAM_SCORE_PATTERN,
+    re.compile(r'predicate_found_in_data_[\w\d_]+'),
 ]
-
-
-SCORING_GRAMMAR_MISUSE_FEATURES = [
-    # scoring
-    'identical_scoring_children_found',
-    'redundant_scoring_terminal_expression_found',
-    'unnecessary_scoring_terminal_expression_found',
-    'total_score_non_positive',
-    'scoring_preferences_used_identically',
-    # numbers?
-    'two_number_operation_found',
-]
-
-
-GAME_ELEMENT_DISJOINTNESS_FEATURES = [
-     # disjoint-ness
-    'disjoint_preferences_found',
-    'disjoint_preferences_scoring_terminal_types',
-    'disjoint_preferences_scoring_terminal_predicates',
-    'disjoint_seq_funcs_found',
-    'disjoint_at_end_found',
-    'disjoint_modal_predicates_found',
-    'disjoint_modal_predicates_prop',
-]
-
 
 COUNTING_FEATURES = [
-    re.compile(r'node_count_[\w\d_]+'),
+    # How many preferences are defined
+    re.compile(r'num_preferences_defined_[\d_]+'),
+    # How many modals are under a then
+    re.compile(r'length_of_then_modals_[\w\d_]+'),
+    # Various features related to variable quantifications
+    re.compile(r'max_quantification_count_[\w\d_]+'),
+    re.compile(r'max_number_variables_types_quantified_[\w\d_]+'),
+    # Man and max depth and node count
     re.compile(r'max_depth_[\w\d_]+'),
+    re.compile(r'mean_depth_[\w\d_]+'),
+    re.compile(r'node_count_[\w\d_]+'),
+    re.compile(r'max_width_[\w\d_]+'),
 ]
 
-
-PREF_FORALL_IMPORTANT_FEATURES = [
-    re.compile(r'pref_forall_[\w\d_]+_correct$'),
-    re.compile(r'pref_forall_[\w\d_]+_incorrect$'),
+# A Smaller set of counting features to exclude
+COUNTING_LESS_IMPORTANT_FEATURES = [
+    # 2023-11-23: trying without the feature for how many preferences are defined, appears to not be great
+    # How many preferences are defined
+    re.compile(r'num_preferences_defined_[\d_]+'),
+    # How many modals are under a then
+    re.compile(r'length_of_then_modals_[\w\d_]+'),
+    # Various features related to variable quantifications
+    re.compile(r'max_quantification_count_[\w\d_]+'),
+    re.compile(r'max_number_variables_types_quantified_[\w\d_]+'),  # CONSIDER-RESTORING
+    # Man and max depth and node count -- keep max depth and node count
+    # 2023-11-13 -- trying with the max width, too, so only mean depth removed
+    # 2023-11-20 -- actually, max width seems to induce weird behavior
+    # 2023-11-23 -- trying with max width one more time after some more recent results
+    # 2023-11-28 -- and without, as it's still a little questionable if it helps or hurts more
+    # re.compile(r'node_count_[\w\d_]+'),
+    # re.compile(r'max_depth_[\w\d_]+'),
+    re.compile(r'mean_depth_[\w\d_]+'),
+    re.compile(r'max_width_[\w\d_]+'),  # CONSIDER-RESTORING  # CONSIDER-REMOVING
 ]
-
-
-# COUNTING_FEATURES = [
-#     # How many preferences are defined
-#     re.compile(r'num_preferences_defined_[\d_]+'),
-#     # How many modals are under a then
-#     re.compile(r'length_of_then_modals_[\w\d_]+'),
-#     # Various features related to variable quantifications
-#     re.compile(r'max_quantification_count_[\w\d_]+'),
-#     re.compile(r'max_number_variables_types_quantified_[\w\d_]+'),
-#     # Man and max depth and node count
-#     re.compile(r'max_depth_[\w\d_]+'),
-#     re.compile(r'mean_depth_[\w\d_]+'),
-#     re.compile(r'node_count_[\w\d_]+'),
-#     re.compile(r'max_width_[\w\d_]+'),
-# ]
-
-# # A Smaller set of counting features to exclude
-# COUNTING_LESS_IMPORTANT_FEATURES = [
-#     # 2023-11-23: trying without the feature for how many preferences are defined, appears to not be great
-#     # How many preferences are defined
-#     re.compile(r'num_preferences_defined_[\d_]+'),
-#     # How many modals are under a then
-#     re.compile(r'length_of_then_modals_[\w\d_]+'),
-#     # Various features related to variable quantifications
-#     re.compile(r'max_quantification_count_[\w\d_]+'),
-#     re.compile(r'max_number_variables_types_quantified_[\w\d_]+'),  # CONSIDER-RESTORING
-#     # Man and max depth and node count -- keep max depth and node count
-#     # 2023-11-13 -- trying with the max width, too, so only mean depth removed
-#     # 2023-11-20 -- actually, max width seems to induce weird behavior
-#     # 2023-11-23 -- trying with max width one more time after some more recent results
-#     # 2023-11-28 -- and without, as it's still a little questionable if it helps or hurts more
-#     # re.compile(r'node_count_[\w\d_]+'),
-#     # re.compile(r'max_depth_[\w\d_]+'),
-#     re.compile(r'mean_depth_[\w\d_]+'),
-#     re.compile(r'max_width_[\w\d_]+'),  # CONSIDER-RESTORING  # CONSIDER-REMOVING
-# ]
 
 COUNTING_FEATURES_PATTERN_DICT = {
     # How many preferences are defined
@@ -137,141 +74,146 @@ COUNTING_FEATURES_PATTERN_DICT = {
 }
 
 
-# FORALL_FEATURES = [
-#     re.compile(r'pref_forall_[\w\d_]+_correct$'),
-#     re.compile(r'pref_forall_[\w\d_]+_incorrect$'),
-#     re.compile(r'[\w\d_]+_incorrect_count$'),
-# ]
+FORALL_FEATURES = [
+    re.compile(r'pref_forall_[\w\d_]+_correct$'),
+    re.compile(r'pref_forall_[\w\d_]+_incorrect$'),
+    re.compile(r'[\w\d_]+_incorrect_count$'),
+]
 
 
-# FORALL_LESS_IMPORTANT_FEATURES = [
-#     # re.compile(r'pref_forall_[\w\d_]+_correct$'),
-#     # re.compile(r'pref_forall_[\w\d_]+_incorrect$'),
-#     re.compile(r'[\w\d_]+_incorrect_count$'),
-# ]
+FORALL_LESS_IMPORTANT_FEATURES = [
+    # re.compile(r'pref_forall_[\w\d_]+_correct$'),
+    # re.compile(r'pref_forall_[\w\d_]+_incorrect$'),
+    re.compile(r'[\w\d_]+_incorrect_count$'),
+]
 
 
-# PREDICATE_UNDER_MODAL_FEATURES = [
-#     re.compile(r'predicate_under_modal_[\w\d_]+'),
-# ]
+PREDICATE_UNDER_MODAL_FEATURES = [
+    re.compile(r'predicate_under_modal_[\w\d_]+'),
+]
 
-# PREDICATE_ROLE_FILLER_FEATURES = [
-#     ARG_TYPES_PATTERN
-# ]
+PREDICATE_ROLE_FILLER_FEATURES = [
+    ARG_TYPES_PATTERN
+]
 
-# PREDICATE_ROLE_FILLER_PATTERN_DICT = {
-#     pred: re.compile(f'{pred}_arg_types_[\\w_]+')
-#     for pred in COMMON_SENSE_PREDICATES_FUNCTIONS
-# }
+PREDICATE_ROLE_FILLER_PATTERN_DICT = {
+    pred: re.compile(f'{pred}_arg_types_[\\w_]+')
+    for pred in COMMON_SENSE_PREDICATES_FUNCTIONS
+}
 
-# COMPOSITIONALITY_FEATURES = [
-#     re.compile(r'compositionality_structure_\d+'),
-# ]
+COMPOSITIONALITY_FEATURES = [
+    re.compile(r'compositionality_structure_\d+'),
+]
 
-# GRAMMAR_USE_FEATURES = [
-#     'variables_defined_all',
-#     'variables_defined_prop',
+GRAMMAR_USE_FEATURES = [
+    'variables_defined_all',
+    'variables_defined_prop',
 
-#     'setup_objects_used',
-#     'setup_quantified_objects_used',
-#     'any_setup_objects_used',
-#     'adjacent_once_found',
-#     'adjacent_same_modal_found',
-#     'starts_and_ends_once',
-#     'once_in_middle_of_pref_found',
-#     'pref_without_hold_found',
-#     'at_end_found',
+    'setup_objects_used',
+    'setup_quantified_objects_used',
+    'any_setup_objects_used',
+    'adjacent_once_found',
+    'adjacent_same_modal_found',
+    'starts_and_ends_once',
+    'once_in_middle_of_pref_found',
+    'pref_without_hold_found',
+    'at_end_found',
 
-#     'nested_logicals_found',
-#     'identical_logical_children_found',
-#     'identical_scoring_children_found',
-#     'scoring_count_expression_repetitions_exist',
-#     'tautological_expression_found',
-#     'redundant_expression_found',
-#     'redundant_scoring_terminal_expression_found',
-#     'unnecessary_expression_found',
-#     'unnecessary_scoring_terminal_expression_found',
-#     'identical_consecutive_seq_func_predicates_found',
-#     'disjoint_preferences_found',
-#     'disjoint_preferences_prop',
-#     'disjoint_preferences_scoring_terminal_types',
-#     'disjoint_preferences_scoring_terminal_predicates',
-#     'disjoint_preferences_same_predicates_only',
-#     'disjoint_seq_funcs_found',
-#     'disjoint_at_end_found',
-#     'disjoint_modal_predicates_found',
-#     'disjoint_modal_predicates_prop',
-#     'predicate_without_variables_or_agent',
+    'nested_logicals_found',
+    'identical_logical_children_found',
+    'identical_scoring_children_found',
+    'scoring_count_expression_repetitions_exist',
+    'tautological_expression_found',
+    'redundant_expression_found',
+    'redundant_scoring_terminal_expression_found',
+    'unnecessary_expression_found',
+    'unnecessary_scoring_terminal_expression_found',
+    'identical_consecutive_seq_func_predicates_found',
+    'disjoint_preferences_found',
+    'disjoint_preferences_prop',
+    'disjoint_preferences_scoring_terminal_types',
+    'disjoint_preferences_scoring_terminal_predicates',
+    'disjoint_preferences_same_predicates_only',
+    'disjoint_seq_funcs_found',
+    'disjoint_at_end_found',
+    'disjoint_modal_predicates_found',
+    'disjoint_modal_predicates_prop',
+    'predicate_without_variables_or_agent',
 
-#     'two_number_operation_found',
-#     'single_argument_multi_operation_found',
+    'two_number_operation_found',
+    'single_argument_multi_operation_found',
 
-#     'section_without_pref_or_total_count_terminal',
-#     'section_without_pref_or_total_count_scoring',
+    'section_without_pref_or_total_count_terminal',
+    'section_without_pref_or_total_count_scoring',
 
-#     'total_score_non_positive',
-#     'scoring_preferences_used_identically'
-# ]
+    'total_score_non_positive',
+    'scoring_preferences_used_identically'
+]
 
-# # The ones that are kept are the ones commented out
-# GRAMMAR_USE_LESS_IMPORTANT_FEATURES = [
-#     # Trying without these, since the minor context-fixing we do now handles this
-#     'variables_defined_all',
-#     'variables_defined_prop',
+# The ones that are kept are the ones commented out
+GRAMMAR_USE_LESS_IMPORTANT_FEATURES = [
+    # Trying without these, since the minor context-fixing we do now handles this
+    'variables_defined_all',
+    'variables_defined_prop',
 
-#     # 2023-11-23: trying without the feature for any setup objects used
-#     'setup_objects_used',
-#     # 2023-09-21: trying without the specific feature to the quantified objects
-#     # 2023-11-22: trying with it again
-#     # 'setup_quantified_objects_used',
-#     # 'any_setup_objects_used,
-#     # 'adjacent_once_found',
-#     # 'adjacent_same_modal_found',
-#     'starts_and_ends_once',
-#     # 2023-11-20: trying with this feature
-#     # 'once_in_middle_of_pref_found',
-#     # 'pref_without_hold_found',  # CONSIDER-RESTORING
-#     # 2023-09-21: trying without an explicit marking of at_end
-#     'at_end_found',
+    # 2023-11-23: trying without the feature for any setup objects used
+    'setup_objects_used',
+    # 2023-09-21: trying without the specific feature to the quantified objects
+    # 2023-11-22: trying with it again
+    # 'setup_quantified_objects_used',
+    # 'any_setup_objects_used,
+    # 'adjacent_once_found',
+    # 'adjacent_same_modal_found',
+    'starts_and_ends_once',
+    # 2023-11-20: trying with this feature
+    # 'once_in_middle_of_pref_found',
+    # 'pref_without_hold_found',  # CONSIDER-RESTORING
+    # 2023-09-21: trying without an explicit marking of at_end
+    'at_end_found',
 
-#     # 'nested_logicals_found',  # CONSIDER-RESTORING
-#     # 'identical_logical_children_found',  # CONSIDER-RESTORING
-#     # 'identical_scoring_children_found',  # CONSIDER-RESTORING
-#     'scoring_count_expression_repetitions_exist',
-#     'tautological_expression_found',  # CONSIDER-RESTORING
-#     # 'redundant_expression_found',
-#     # 'redundant_scoring_terminal_expression_found',
-#     # 'unnecessary_expression_found',
-#     # 'unnecessary_scoring_terminal_expression_found',
-#     # 'identical_consecutive_seq_func_predicates_found',
-#     # 'disjoint_preferences_found',
-#     'disjoint_preferences_prop',   #  CONSIDER-REMOVING
-#     # 'disjoint_preferences_scoring_terminal_types',
-#     # 'disjoint_preferences_scoring_terminal_predicates', # CONSIDER-REMOVING
-#     'disjoint_preferences_same_predicates_only',  # CONSIDER-REMOVING
-#     # 'disjoint_seq_funcs_found',
-#     # 'disjoint_at_end_found',
-#     # 'disjoint_modal_predicates_found',  # CONSIDER-REMOVING
-#     # 'disjoint_modal_predicates_prop',
-#     # 'predicate_without_variables_or_agent',
+    # 'nested_logicals_found',  # CONSIDER-RESTORING
+    # 'identical_logical_children_found',  # CONSIDER-RESTORING
+    # 'identical_scoring_children_found',  # CONSIDER-RESTORING
+    'scoring_count_expression_repetitions_exist',
+    'tautological_expression_found',  # CONSIDER-RESTORING
+    # 'redundant_expression_found',
+    # 'redundant_scoring_terminal_expression_found',
+    # 'unnecessary_expression_found',
+    # 'unnecessary_scoring_terminal_expression_found',
+    # 'identical_consecutive_seq_func_predicates_found',
+    # 'disjoint_preferences_found',
+    'disjoint_preferences_prop',   #  CONSIDER-REMOVING
+    # 'disjoint_preferences_scoring_terminal_types',
+    # 'disjoint_preferences_scoring_terminal_predicates', # CONSIDER-REMOVING
+    'disjoint_preferences_same_predicates_only',  # CONSIDER-REMOVING
+    # 'disjoint_seq_funcs_found',
+    # 'disjoint_at_end_found',
+    # 'disjoint_modal_predicates_found',  # CONSIDER-REMOVING
+    # 'disjoint_modal_predicates_prop',
+    # 'predicate_without_variables_or_agent',
 
-#     # 'two_number_operation_found',
-#     'single_argument_multi_operation_found',
+    # 'two_number_operation_found',
+    'single_argument_multi_operation_found',
 
-#     'section_without_pref_or_total_count_terminal', # CONSIDER-REMOVING
-#     'section_without_pref_or_total_count_scoring',  # CONSIDER-REMOVING
+    'section_without_pref_or_total_count_terminal', # CONSIDER-REMOVING
+    'section_without_pref_or_total_count_scoring',  # CONSIDER-REMOVING
 
-#     # 'total_score_non_positive',
-#     # 'scoring_preferences_used_identically'
-# ]
-
-FEATURES_SUFFIX = '_FEATURES'
-
+    # 'total_score_non_positive',
+    # 'scoring_preferences_used_identically'
+]
 
 FEATURE_CATEGORIES = {
-    local_key.replace(FEATURES_SUFFIX, '').lower(): local_value
-    for local_key, local_value in locals().items()
-    if local_key.endswith(FEATURES_SUFFIX)
+    'grammar_context': GRAMMAR_CONTEXT_FEATURES,
+    'ngram_and_data_based': NGRAM_AND_DATA_BASED_FEATURES,
+    'counting': COUNTING_FEATURES,
+    'counting_less_important': COUNTING_LESS_IMPORTANT_FEATURES,
+    'forall': FORALL_FEATURES,
+    'forall_less_important': FORALL_LESS_IMPORTANT_FEATURES,
+    'predicate_under_modal': PREDICATE_UNDER_MODAL_FEATURES,
+    'predicate_role_filler': PREDICATE_ROLE_FILLER_FEATURES,
+    'compositionality': COMPOSITIONALITY_FEATURES,
+    'grammar_use': GRAMMAR_USE_FEATURES,
+    'grammar_use_less_important': GRAMMAR_USE_LESS_IMPORTANT_FEATURES,
 }
 
 
